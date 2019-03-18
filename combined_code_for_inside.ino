d59f05c3e61b1748f1dd2948252d8aa0f512fdaa
Bhaskar <39699452+bhaskarsdose@users.noreply.github.com>
2019-03-19 00:05:41 +0530

    Add files via upload

---
+++ combined_code_for_inside.ino
@@ -0,0 +1,493 @@
+//made by bhaskar :))
+
+// Loading the ESP8266WiFi library and the PubSubClient library
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+float score1;
+static char IAQtext[25]; 
+//For bme680
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+
+//for dust sensor
+#define USE_AVG
+// Arduino pin numbers.
+const int sharpLEDPin = D4;   // Nodemcu digital pin 1 connect to sensor LED.
+const int sharpVoPin = A0;   // Nodemcu analog pin 1 connect to sensor Vo.
+// For averaging last N raw voltage readings.
+#ifdef USE_AVG
+#define N 1
+static unsigned long VoRawTotal = 0;
+static int VoRawCount = 0;
+#endif // USE_AVG
+// Set the typical output voltage in Volts when there is zero dust. 
+static float Voc = 0.6;
+
+// Use the typical sensitivity in units of V per 100ug/m3.
+const float K = 0.5;
+
+
+
+
+
+
+Adafruit_BME680 bme; // I2C
+float hum_weighting = 0.25; // so hum effect is 25% of the total air quality score
+float gas_weighting = 0.75; // so gas effect is 75% of the total air quality score
+
+float hum_score, gas_score;
+float gas_reference = 250000;
+float hum_reference = 40;
+int   getgasreference_count = 0;
+
+// Uncomment one of the lines bellow for whatever DHT sensor type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+// Change the credentials below, so your ESP8266 connects to your router
+const char* ssid = "power8";
+const char* password = "hellohello";
+
+// Change the variable to your Raspberry Pi IP address, so it connects to your MQTT broker
+const char* mqtt_server = "192.168.0.102";
+
+// Initializes the espClient
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Connect an LED to each GPIO of your ESP8266
+const int ledGPIO5 = 14;
+const int ledGPIO4 = 12;
+
+// DHT Sensor
+//const int DHTPin = 2;
+
+// Initialize DHT sensor.
+//DHT dht(DHTPin, DHTTYPE);
+
+// Timers auxiliar variables
+long now = millis();
+long lastMeasure = 0;
+
+// Don't change the function below. This functions connects your ESP8266 to your router
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// This functions is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
+// Change the function below to add logic to your program, so when a device publishes a message to a topic that 
+// your ESP8266 is subscribed you can actually do something
+void callback(String topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic home/office/esp1/gpio2, you check if the message is either 1 or 0. Turns the ESP GPIO according to the message
+  if(topic=="esp8266/4"){
+      Serial.print("Changing GPIO 4 to ");
+      if(messageTemp == "1"){
+        digitalWrite(ledGPIO4, HIGH);
+        Serial.print("On");
+      }
+      else if(messageTemp == "0"){
+        digitalWrite(ledGPIO4, LOW);
+        Serial.print("Off");
+      }
+  }
+  if(topic=="esp8266/5"){
+      Serial.print("Changing GPIO 5 to ");
+      if(messageTemp == "1"){
+        digitalWrite(ledGPIO5, HIGH);
+        Serial.print("On");
+      }
+      else if(messageTemp == "0"){
+        digitalWrite(ledGPIO5, LOW);
+        Serial.print("Off");
+      }
+  }
+  Serial.println();
+}
+
+// This functions reconnects your ESP8266 to your MQTT broker
+// Change the function below if you want to subscribe to more topics with your ESP8266 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+     /*
+     YOU  NEED TO CHANGE THIS NEXT LINE, IF YOU'RE HAVING PROBLEMS WITH MQTT MULTIPLE CONNECTIONS
+     To change the ESP device ID, you will have to give a unique name to the ESP8266.
+     Here's how it looks like now:
+       if (client.connect("ESP8266Client")) {
+     If you want more devices connected to the MQTT broker, you can do it like this:
+       if (client.connect("ESPOffice")) {
+     Then, for the other ESP:
+       if (client.connect("ESPGarage")) {
+      That should solve your MQTT multiple connections problem
+
+     THE SECTION IN loop() function should match your device name
+    */
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");  
+      // Subscribe or resubscribe to a topic
+      // You can subscribe to more topics (to control more LEDs in this example)
+      client.subscribe("esp8266/4");
+      client.subscribe("esp8266/5");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+// The setup function sets your ESP GPIOs to Outputs, starts the serial communication at a baud rate of 115200
+// Sets your mqtt broker and sets the callback function
+// The callback function is what receives messages and actually controls the LEDs
+void setup() {
+//Setup for dust sensor
+// Set LED pin for output.
+  pinMode(sharpLEDPin, OUTPUT);
+  delay(2000);
+  Serial.println("");
+  Serial.println("GP2Y1014AU0F Demo");
+  Serial.println("=================");
+
+  
+ 
+ // pinMode(ledGPIO4, OUTPUT);
+ // pinMode(ledGPIO5, OUTPUT);
+  
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println(F("BME680 test"));
+  Wire.begin();
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    while (1);
+  } else Serial.println("Found a sensor");
+
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_2X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_2X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320°C for 150 ms
+  // Now run the sensor for a burn-in period, then use combination of relative humidity and gas resistance to estimate indoor air quality as a percentage.
+  GetGasReference();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+//functions for dust sensor
+
+// Helper functions to print a data value to the serial monitor.
+void printValue(String text, unsigned int value, bool isLast = false) {
+  Serial.print(text);
+  Serial.print("=");
+  Serial.print(value);
+  if (!isLast) {
+    Serial.print(", ");
+  }
+}
+void printFValue(String text, float value, String units, bool isLast = false) {
+  Serial.print(text);
+  Serial.print("=");
+  Serial.print(value);
+  Serial.print(units);
+  if (!isLast) {
+    Serial.print(", ");
+  }
+}
+
+
+
+// For this project, you don't need to change anything in the loop function. 
+// Basically it ensures that you ESP is connected to your broker
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  if(!client.loop())
+     /*
+     YOU  NEED TO CHANGE THIS NEXT LINE, IF YOU'RE HAVING PROBLEMS WITH MQTT MULTIPLE CONNECTIONS
+     To change the ESP device ID, you will have to give a unique name to the ESP8266.
+     Here's how it looks like now:
+       client.connect("ESP8266Client");
+     If you want more devices connected to the MQTT broker, you can do it like this:
+       client.connect("ESPOffice");
+     Then, for the other ESP:
+       client.connect("ESPGarage");
+      That should solve your MQTT multiple connections problem
+
+     THE SECTION IN recionnect() function should match your device name
+    */
+    client.connect("ESP8266Client");
+
+
+
+//dust sensor
+// Turn on the dust sensor LED by setting digital pin LOW.
+  
+  
+  
+  digitalWrite(sharpLEDPin, LOW);
+
+  // Wait 0.28ms before taking a reading of the output voltage as per spec.
+  delayMicroseconds(280);
+
+  // Record the output voltage. This operation takes around 100 microseconds.
+  int VoRaw = analogRead(sharpVoPin);
+  
+  // Turn the dust sensor LED off by setting digital pin HIGH.
+  digitalWrite(sharpLEDPin, HIGH);
+
+  // Wait for remainder of the 10ms cycle = 10000 - 280 - 100 microseconds.
+  delayMicroseconds(9620);
+  
+  // Print raw voltage value (number from 0 to 1023).
+  #ifdef PRINT_RAW_DATA
+  printValue("VoRaw", VoRaw, true);
+  Serial.println("");
+  #endif // PRINT_RAW_DATA
+
+
+
+float Vo = VoRaw;
+  #ifdef USE_AVG
+  VoRawTotal += VoRaw;
+  VoRawCount++;
+  if ( VoRawCount >= N ) {
+    Vo = 1.0 * VoRawTotal / N;
+    VoRawCount = 0;
+    VoRawTotal = 0;
+  } else {
+    return;
+  }
+  #endif // USE_AVG
+   Vo = Vo / 1024.0 * 5.0;
+  printFValue("Vo", Vo*1000.0, "mV");
+
+  // Convert to Dust Density in units of ug/m3.
+  float dV = Vo - Voc;
+  if ( dV < 0 ) {
+    dV = 0;
+    Voc = Vo;
+  }
+  float dustDensity = dV / K * 100.0;
+  printFValue("DustDensity", dustDensity, "ug/m3", true);
+  Serial.println("");
+
+  static char PM[7];
+    dtostrf(dustDensity, 6, 2, PM);
+    client.publish("/esp8266/PM", PM);
+
+
+//last of dust sensor
+
+
+
+    
+    
+  now = millis();
+  // Publishes new temperature and humidity every 10 seconds
+  if (now - lastMeasure > 1000) {
+    lastMeasure = now;
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = bme.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = bme.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //float f = dht.readTemperature(true);
+    float p = bme.readPressure();
+   // float g = bme.readGas();
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(t) || isnan(p) || isnan(h) ) {
+      Serial.println("Failed to read from BME680 sensor!");
+      return;
+    }
+    
+    
+    // Computes temperature values in Celsius
+    //float hic = dht.computeHeatIndex(t, h, false);
+    static char temperature[7];
+    dtostrf(t, 6, 2, temperature);
+    
+    // Uncomment to compute temperature values in Fahrenheit 
+    // float hif = dht.computeHeatIndex(f, h);
+    // static char temperatureTemp[7];
+    // dtostrf(hic, 6, 2, temperatureTemp);
+    
+    static char humidity[7];
+    dtostrf(h, 6, 2, humidity);
+
+    client.publish("/esp8266/humidity", humidity);
+    static char pressure[7];
+    dtostrf(p, 6, 2, pressure);
+
+  
+    static char AQIscore[7];
+    dtostrf(score1, 6, 2, AQIscore);
+    Serial.println(score1);
+  
+    client.publish("/esp8266/AQIscore", AQIscore);   //publishes the AQI score or value to the server
+
+    //static char AQItext[7];
+    //dtostrf(AQI_text1, 6, 2, AQItext);
+    client.publish("/esp8266/AQItext", IAQtext);
+    Serial.println(IAQtext);
+
+    // Publishes Temperature and Humidity values
+    client.publish("/esp8266/temperature", temperature);
+    
+    client.publish("/esp8266/pressure", pressure);
+
+    
+
+
+    
+    /*Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t Temperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print(f);
+    Serial.print(" *F\t Heat index: ");
+    Serial.print(hic);
+    Serial.println(" *C ");*/
+
+     Serial.print("Temperature = ");
+
+  Serial.print(bme.readTemperature());
+  Serial.println("°C");
+
+  Serial.print("   Pressure = ");
+
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.print("   Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println("%");
+
+  Serial.print("        Gas = ");
+  Serial.print(bme.readGas());
+  Serial.println("R\n");
+    // Serial.print(hif);
+    // Serial.println(" *F");
+//Calculate humidity contribution to IAQ index
+  float current_humidity = bme.readHumidity();
+  if (current_humidity >= 38 && current_humidity <= 42)
+    hum_score = 0.25*100; // Humidity +/-5% around optimum 
+  else
+  { //sub-optimal
+    if (current_humidity < 38) 
+      hum_score = 0.25/hum_reference*current_humidity*100;
+    else
+    {
+      hum_score = ((-0.25/(100-hum_reference)*current_humidity)+0.416666)*100;
+    }
+  }
+
+  //Calculate gas contribution to IAQ index
+  float gas_lower_limit = 5000;   // Bad air quality limit
+  float gas_upper_limit = 50000;  // Good air quality limit 
+  if (gas_reference > gas_upper_limit) gas_reference = gas_upper_limit; 
+  if (gas_reference < gas_lower_limit) gas_reference = gas_lower_limit;
+  gas_score = (0.75/(gas_upper_limit-gas_lower_limit)*gas_reference -(gas_lower_limit*(0.75/(gas_upper_limit-gas_lower_limit))))*100;
+  
+  //Combine results for the final IAQ index value (0-100% where 100% is good quality air)
+  float air_quality_score = hum_score + gas_score;
+
+  Serial.println("Air Quality = "+String(air_quality_score,1)+"% derived from 25% of Humidity reading and 75% of Gas reading - 100% is good quality air");
+  Serial.println("Humidity element was : "+String(hum_score/100)+" of 0.25");
+  Serial.println("     Gas element was : "+String(gas_score/100)+" of 0.75");
+  if (bme.readGas() < 120000) Serial.println("***** Poor air quality *****");
+  Serial.println();
+  if ((getgasreference_count++)%10==0) GetGasReference(); 
+//  Serial.print(score);
+  Serial.println(CalculateIAQ(air_quality_score));
+  Serial.println("------------------------------------------------");
+  delay(2000);
+    Serial.println("i am here bhaskar");
+
+
+    
+  }
+
+
+
+  
+}//End of main loop()
+
+
+
+void GetGasReference(){
+  // Now run the sensor for a burn-in period, then use combination of relative humidity and gas resistance to estimate indoor air quality as a percentage.
+  Serial.println("Getting a new gas reference value");
+  int readings = 10;
+  for (int i = 0; i <= readings; i++){ // read gas for 10 x 0.150mS = 1.5secs
+    gas_reference += bme.readGas();
+  }
+  gas_reference = gas_reference / readings;
+}
+
+String CalculateIAQ(float score){
+  String IAQ_text = "Air quality is ";
+  score = (100-score)*5;
+  score1 = score;
+  Serial.print(score);
+  if      (score >= 301)                  IAQ_text += "Hazardous";
+  else if (score >= 201 && score <= 300 ) IAQ_text += "Very Unhealthy";
+  else if (score >= 176 && score <= 200 ) IAQ_text += "Unhealthy";
+  else if (score >= 151 && score <= 175 ) IAQ_text += "Unhealthy for Sensitive Groups";
+  else if (score >=  51 && score <= 150 ) IAQ_text += "Moderate";
+  else if (score >=  00 && score <=  50 ) IAQ_text += "Good";
+  
+  int i;
+  for(i=0;i<=sizeof(IAQtext);i++)
+  {IAQtext[i] = IAQ_text[i];}
+  
+  return IAQ_text;
+}
+
+
+
+
